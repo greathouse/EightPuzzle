23672bd6f6ab510f7a8127e7ed3621dc0bcd8376
Robert Greathouse <greathouse.robert@gmail.com>
2012-05-23 20:03:35 -0500

    Fixing tests

--- spock/SolverTests.groovy
+++ spock/SolverTests.groovy
@@ -7,28 +7,29 @@ class SolverTests extends spock.lang.Specification {
       def solver = new Solver("1234567890")
     
     when:      
-//      def result = solver.solve()
-      println "hi"
+      def result = solver.solve()
     
     then:
-      1 == 1
-//      result != null
-//      result.moves == 0
-//      result.calculations == 1
+      result != null
+      result.moves == 0
+      result.calculations == 1
   }
   
-  def "simple test"() {
+  def "simple test to validate equal or better performance during refactoring"() {
     setup:
-      def solver = new Solver("012345678")
+      def solver = new Solver(puzzle)
     
     when:
-      //def result = solver.solve()
-      println "hi"
+      def result = solver.solve()
       
     then:
-      1 == 1
-//      result != null
-//      result.moves == 1
-//      result.calculations == 2
+      result != null
+      result.moves <= moves
+      result.calculations <= calculations
+    where:
+      puzzle      | moves | calculations
+      "123450786" | 1     | 2
+      "123405786" | 2     | 3
+      "012345678" | 76    | 199
   }
 }
