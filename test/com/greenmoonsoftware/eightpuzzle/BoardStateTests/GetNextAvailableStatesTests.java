a2a50ad99a7a59fd416d40f5e176080f47309e5a
Robert Greathouse <greathouse.robert@gmail.com>
2012-05-24 20:28:57 -0500

    Finished adding 8-puzzle "next available state" tests

--- test/com/greenmoonsoftware/eightpuzzle/BoardStateTests/GetNextAvailableStatesTests.java
+++ test/com/greenmoonsoftware/eightpuzzle/BoardStateTests/GetNextAvailableStatesTests.java
@@ -40,13 +40,33 @@ public class GetNextAvailableStatesTests extends TestBase {
     run("1 2 0 3 4 5 6 7 8", 2, new String[]{"1 0 2 3 4 5 6 7 8", "1 2 5 3 4 0 6 7 8"});
   }
   
+  public void testMiddleLeft() {
+    setup8();
+    run("1 2 3 0 4 5 6 7 8", 3, new String[]{"0 2 3 1 4 5 6 7 8", "1 2 3 4 0 5 6 7 8", "1 2 3 6 4 5 0 7 8"});
+  }
+  
   public void testCenter() {
     setup8();
     run("1 2 3 4 0 5 6 7 8", 4, new String[]{"1 0 3 4 2 5 6 7 8", "1 2 3 0 4 5 6 7 8", "1 2 3 4 5 0 6 7 8", "1 2 3 4 7 5 6 0 8"});
   }
   
+  public void testMiddleRight() {
+    setup8();
+    run("1 2 3 4 5 0 6 7 8", 3, new String[]{"1 2 0 4 5 3 6 7 8", "1 2 3 4 0 5 6 7 8", "1 2 3 4 5 8 6 7 0"});
+  }
+  
   public void testBottomLeft() {
     setup8();
     run("1 2 3 4 5 6 0 7 8", 2, new String[]{"1 2 3 0 5 6 4 7 8", "1 2 3 4 5 6 7 0 8"});
+  }
+  
+  public void testBottomCenter() {
+    setup8();
+    run("1 2 3 4 5 6 7 0 8", 3, new String[]{"1 2 3 4 0 6 7 5 8", "1 2 3 4 5 6 0 7 8", "1 2 3 4 5 6 7 8 0"});
+  }
+  
+  public void testBottomRight() {
+    setup8();
+    run("1 2 3 4 5 6 7 8 0", 2, new String[]{"1 2 3 4 5 0 7 8 6", "1 2 3 4 5 6 7 0 8"});
   }
 }
